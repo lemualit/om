204e41bf09c06d13d08f969023caa75d045bd1d9
Lemual <lemuality@gmail.com>
2023-10-26 18:18:21 +0700

    5
    5

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-window.addEventListener('load', function () {
-    const loader = document.querySelector('.loader');
-    loader.classList.add('hidden');
-  });
+let btnOb = document.querySelector('.btnOb')
+let nav_link = documen.querySelector('.nav')
+
+function changeClass() {
+    nav_link.classList.remove("nav"); // Удаляем текущий класс
+    nav_link.classList.add("nav_link_zero"); // Добавляем новый класс
+}
+
+myButton.addEventListener("click", changeClass());
